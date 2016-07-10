3dc32082ad612a4f8e3237a050e783c8bb1ff695
Modesto San Juan <modestosja@gmail.com>
2016-07-10 04:37:20 +0100

    Added EventBus

---
+++ RpgCombatKata.Core/EventBus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reactive.Subjects;
+
+namespace RpgCombatKata.Core
+{
+    public class EventBus : IDisposable
+    {
+        private readonly ConcurrentDictionary<Type, object> busDictionary = new ConcurrentDictionary<Type, object>();
+
+        public virtual IObservable<T> Subscriber<T>()
+        {
+            return GetBus<T>();
+        }
+
+        public virtual void Publish<T>(T msg)
+        {
+            GetBus<T>().OnNext(msg);
+        }
+
+        private ISubject<T> GetBus<T>()
+        {
+            var bus = busDictionary.GetOrAdd(typeof(T), (type) => new Subject<T>());
+            return bus as ISubject<T>;
+        }
+
+        public void Dispose() {
+            foreach (var bus in busDictionary.Values) {
+                (bus as IDisposable)?.Dispose();
+            }
+        }
+    }
+}
---
+++ RpgCombatKata.Tests/EventBusShould.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using RpgCombatKata.Core;
+
+namespace RpgCombatKata.Tests
+{
+    [TestFixture]
+    public class EventBusShould {
+        private EventBus eventBus;
+
+        [SetUp]
+        public void SetUp() {
+            eventBus = new EventBus();
+        }
+
+        [TearDown]
+        public void TearDown() {
+            eventBus.Dispose();
+        }
+
+        [Test]
+        public void allow_to_subscribe_by_type() {
+            var value = 0;
+            using (eventBus.Subscriber<int>().Subscribe(x => value = x)) {
+                eventBus.Publish(1);
+                value.Should().Be(1);
+            }
+        }
+
+        [Test]
+        public void dispose_subscriber()
+        {
+            var value = 0;
+            eventBus.Subscriber<int>().Subscribe(x => value = x).Dispose();
+            eventBus.Publish(1);
+            value.Should().Be(0);
+        }
+    }
+}
