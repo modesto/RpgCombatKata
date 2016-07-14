9e8e50d6fac3c30f26adbcf3b4052a39eee4e122
Modesto San Juan <modestosja@gmail.com>
2016-07-14 01:53:24 +0100

    Extract MapBasedCombatRules tests

--- RpgCombatKata.Tests/Rules/CombatRulesShould.cs
+++ RpgCombatKata.Tests/Rules/CombatRulesShould.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.Generic;
 using FluentAssertions;
-using NSubstitute;
 using NUnit.Framework;
 using RpgCombatKata.Core.Model.Characters;
 using RpgCombatKata.Core.Model.Combat;
-using RpgCombatKata.Core.Model.Map;
 
 namespace RpgCombatKata.Tests.Rules
 {
@@ -67,79 +65,6 @@ namespace RpgCombatKata.Tests.Rules
             var expectedDamage = (int)(damage - (damage * 0.5));
             When.TriedToAttack(attacker.Id, defender.Id, damage: damage);
             defender.HealthCondition.CurrentHealth.Should().Be(initialHealth - expectedDamage);
-        }
-
-        [Test]
-        public void allow_melee_attacks_in_range()
-        {
-            var attacker = Given.ALiveCharacter();
-            var defender = Given.ALiveCharacter();
-            var gameMap = Given.AGameMap();
-            gameMap.DistanceBetween(attacker.Id, defender.Id).Returns(Distance.FromMeters(2));
-            var mapRules = Given.AMapBasedCombatRules(gameMap);
-            var rulesEngine = Given.ARulesEngine(mapRules);
-            var initialHealth = defender.HealthCondition.CurrentHealth;
-            var damage = 100;
-            When.TriedToAttack(attacker.Id, defender.Id, damage: damage);
-            defender.HealthCondition.CurrentHealth.Should().Be(initialHealth - damage);
-        }
-
-        [Test]
-        public void forbide_melee_attacks_out_of_range()
-        {
-            var attacker = Given.ALiveCharacter();
-            var defender = Given.ALiveCharacter();
-            var gameMap = Given.AGameMap();
-            gameMap.DistanceBetween(attacker.Id, defender.Id).Returns(Distance.FromMeters(3));
-            var mapRules = Given.AMapBasedCombatRules(gameMap);
-            var rulesEngine = Given.ARulesEngine(mapRules);
-            var initialHealth = defender.HealthCondition.CurrentHealth;
-            var damage = 100;
-            When.TriedToAttack(attacker.Id, defender.Id, damage: damage, kind: AttackRanges.Melee());
-            defender.HealthCondition.CurrentHealth.Should().Be(initialHealth);
-        }
-
-        [Test]
-        public void allow_range_attacks_in_range()
-        {
-            var attacker = Given.ALiveCharacter();
-            var defender = Given.ALiveCharacter();
-            var gameMap = Given.AGameMap();
-            gameMap.DistanceBetween(attacker.Id, defender.Id).Returns(Distance.FromMeters(20));
-            var mapRules = Given.AMapBasedCombatRules(gameMap);
-            var rulesEngine = Given.ARulesEngine(mapRules);
-            var initialHealth = defender.HealthCondition.CurrentHealth;
-            var damage = 100;
-            When.TriedToAttack(attacker.Id, defender.Id, damage: damage, kind: AttackRanges.Range());
-            defender.HealthCondition.CurrentHealth.Should().Be(initialHealth- damage);
-        }
-
-        [Test]
-        public void forbide_range_attacks_out_of_range()
-        {
-            var attacker = Given.ALiveCharacter();
-            var defender = Given.ALiveCharacter();
-            var gameMap = Given.AGameMap();
-            gameMap.DistanceBetween(attacker.Id, defender.Id).Returns(Distance.FromMeters(21));
-            var mapRules = Given.AMapBasedCombatRules(gameMap);
-            var rulesEngine = Given.ARulesEngine(mapRules);
-            var initialHealth = defender.HealthCondition.CurrentHealth;
-            var damage = 100;
-            When.TriedToAttack(attacker.Id, defender.Id, damage: damage, kind: AttackRanges.Range());
-            defender.HealthCondition.CurrentHealth.Should().Be(initialHealth);
-        }
-
-        [Test]
-        public void forbide_a_character_to_heal_an_enemy()
-        {
-            var factionCombatRules = Given.AFactionCombatRules();
-            var rulesEngine = Given.ARulesEngine(factionCombatRules);
-            var healer = Given.ALiveCharacter();
-            var enemy = Given.ALiveCharacter(healthPoints: 900);
-            var initialHealth = enemy.HealthCondition.CurrentHealth;
-            var pointsToHeal = 50;
-            When.TriedToHeal(healer.Id, enemy.Id, heal: pointsToHeal);
-            enemy.HealthCondition.CurrentHealth.Should().Be(initialHealth);
         }
 
 
---
+++ RpgCombatKata.Tests/Rules/MapBasedCombatRulesShould.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using RpgCombatKata.Core.Model.Map;
+
+namespace RpgCombatKata.Tests.Rules
+{
+    [TestFixture]
+    public class MapBasedCombatRulesShould : TestBase
+    {
+
+        [Test]
+        public void allow_melee_attacks_in_range()
+        {
+            var attacker = Given.ALiveCharacter();
+            var defender = Given.ALiveCharacter();
+            var gameMap = Given.AGameMap();
+            gameMap.DistanceBetween(attacker.Id, defender.Id).Returns(Distance.FromMeters(2));
+            var mapRules = Given.AMapBasedCombatRules(gameMap);
+            var rulesEngine = Given.ARulesEngine(mapRules);
+            var initialHealth = defender.HealthCondition.CurrentHealth;
+            var damage = 100;
+            When.TriedToAttack(attacker.Id, defender.Id, damage: damage);
+            defender.HealthCondition.CurrentHealth.Should().Be(initialHealth - damage);
+        }
+
+        [Test]
+        public void forbide_melee_attacks_out_of_range()
+        {
+            var attacker = Given.ALiveCharacter();
+            var defender = Given.ALiveCharacter();
+            var gameMap = Given.AGameMap();
+            gameMap.DistanceBetween(attacker.Id, defender.Id).Returns(Distance.FromMeters(3));
+            var mapRules = Given.AMapBasedCombatRules(gameMap);
+            var rulesEngine = Given.ARulesEngine(mapRules);
+            var initialHealth = defender.HealthCondition.CurrentHealth;
+            var damage = 100;
+            When.TriedToAttack(attacker.Id, defender.Id, damage: damage, kind: AttackRanges.Melee());
+            defender.HealthCondition.CurrentHealth.Should().Be(initialHealth);
+        }
+
+        [Test]
+        public void allow_range_attacks_in_range()
+        {
+            var attacker = Given.ALiveCharacter();
+            var defender = Given.ALiveCharacter();
+            var gameMap = Given.AGameMap();
+            gameMap.DistanceBetween(attacker.Id, defender.Id).Returns(Distance.FromMeters(20));
+            var mapRules = Given.AMapBasedCombatRules(gameMap);
+            var rulesEngine = Given.ARulesEngine(mapRules);
+            var initialHealth = defender.HealthCondition.CurrentHealth;
+            var damage = 100;
+            When.TriedToAttack(attacker.Id, defender.Id, damage: damage, kind: AttackRanges.Range());
+            defender.HealthCondition.CurrentHealth.Should().Be(initialHealth - damage);
+        }
+
+        [Test]
+        public void forbide_range_attacks_out_of_range()
+        {
+            var attacker = Given.ALiveCharacter();
+            var defender = Given.ALiveCharacter();
+            var gameMap = Given.AGameMap();
+            gameMap.DistanceBetween(attacker.Id, defender.Id).Returns(Distance.FromMeters(21));
+            var mapRules = Given.AMapBasedCombatRules(gameMap);
+            var rulesEngine = Given.ARulesEngine(mapRules);
+            var initialHealth = defender.HealthCondition.CurrentHealth;
+            var damage = 100;
+            When.TriedToAttack(attacker.Id, defender.Id, damage: damage, kind: AttackRanges.Range());
+            defender.HealthCondition.CurrentHealth.Should().Be(initialHealth);
+        }
+
+    }
+}
