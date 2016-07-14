2c980f527f93175002fa9145e26b4e0ce9cfbecd
Modesto San Juan <modestosja@gmail.com>
2016-07-14 14:32:31 +0100

    ignore

--- RpgCombatKata.Tests/Rules/LevelBasedCombatRulesShould.cs
+++ RpgCombatKata.Tests/Rules/LevelBasedCombatRulesShould.cs
@@ -13,14 +13,14 @@ namespace RpgCombatKata.Tests.Rules
         public void attacks_do_more_damage_to_low_level_characters() {
             var attacker = Given.ALiveCharacter(level: 10);
             var defender = Given.ALiveCharacter(level: 5);
-            List<Character> charactersStubData = new List<Character>() {attacker, defender};
-            var combatRules = Given.ACombatRules();
-            var levelBasedCombatRules = Given.ALevelBasedCombatRules(charactersStubData);
-            var rules = new List<Core.Business.Rules.Rules>() {combatRules, levelBasedCombatRules};
-            Given.ARulesPipeline(rules);
             var initialHealth = defender.HealthCondition.CurrentHealth;
             var damage = 100;
             var expectedDamage = (int)(damage * 1.5);
+            var charactersStubData = new List<Character>() {attacker, defender};
+            var levelBasedCombatRules = Given.ALevelBasedCombatRules(charactersStubData);
+            var combatRules = Given.ACombatRules();
+            var rules = new List<Core.Business.Rules.Rules>() {combatRules, levelBasedCombatRules};
+            Given.ARulesPipeline(rules);
             When.TriedToAttack(attacker.Id, defender.Id, damage: damage);
             defender.HealthCondition.CurrentHealth.Should().Be(initialHealth - expectedDamage);
         }
@@ -30,14 +30,14 @@ namespace RpgCombatKata.Tests.Rules
         {
             var attacker = Given.ALiveCharacter(level: 5);
             var defender = Given.ALiveCharacter(level: 10);
-            List<Character> charactersStubData = new List<Character>() { attacker, defender };
-            var combatRules = Given.ACombatRules();
-            var levelBasedCombatRules = Given.ALevelBasedCombatRules(charactersStubData);
-            var rules = new List<Core.Business.Rules.Rules>() { combatRules, levelBasedCombatRules };
-            Given.ARulesPipeline(rules);
             var initialHealth = defender.HealthCondition.CurrentHealth;
             var damage = 100;
             var expectedDamage = (int)(damage - (damage * 0.5));
+            var combatRules = Given.ACombatRules();
+            var charactersStubData = new List<Character>() { attacker, defender };
+            var levelBasedCombatRules = Given.ALevelBasedCombatRules(charactersStubData);
+            var rules = new List<Core.Business.Rules.Rules>() { combatRules, levelBasedCombatRules };
+            Given.ARulesPipeline(rules);
             When.TriedToAttack(attacker.Id, defender.Id, damage: damage);
             defender.HealthCondition.CurrentHealth.Should().Be(initialHealth - expectedDamage);
         }
