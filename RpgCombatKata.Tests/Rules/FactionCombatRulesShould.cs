d8c6daf33dd36ef02d72534512142862747a82cf
Modesto San Juan <modestosja@gmail.com>
2016-07-15 23:54:24 +0100

    Extract some variables in faction combat rules tests

--- RpgCombatKata.Tests/Rules/FactionCombatRulesShould.cs
+++ RpgCombatKata.Tests/Rules/FactionCombatRulesShould.cs
@@ -6,30 +6,31 @@ namespace RpgCombatKata.Tests.Rules
     [TestFixture]
     public class FactionCombatRulesShould : TestBase
     {
-
         [Test]
         public void allow_a_character_heal_an_ally()
         {
+            var initialHealth = 900;
+            var pointsToHeal = 50;
             var character = Given.ALiveCharacter();
-            var anAlly = Given.ALiveCharacter(healthPoints: 900);
+            var anAlly = Given.ALiveCharacter(healthPoints: initialHealth);
             var faction = Given.AFaction();
             var factionRules = Given.AFactionCombatRules(faction);
             Given.ARulesPipeline(factionRules);
             When.TriedToJoinFaction(character.Id, faction.Id);
             When.TriedToJoinFaction(anAlly.Id, faction.Id);
-            When.TriedToHeal(character.Id, anAlly.Id, 50);
-            anAlly.HealthCondition.CurrentHealth.Should().Be(950);
+            When.TriedToHeal(character.Id, anAlly.Id, pointsToHeal);
+            anAlly.HealthCondition.CurrentHealth.Should().Be(initialHealth+pointsToHeal);
         }
 
         [Test]
         public void forbide_a_character_to_heal_an_enemy()
         {
+            var initialHealth = 900;
+            var pointsToHeal = 50;
             var factionCombatRules = Given.AFactionCombatRules();
             Given.ARulesPipeline(factionCombatRules);
             var healer = Given.ALiveCharacter();
-            var enemy = Given.ALiveCharacter(healthPoints: 900);
-            var initialHealth = enemy.HealthCondition.CurrentHealth;
-            var pointsToHeal = 50;
+            var enemy = Given.ALiveCharacter(healthPoints: initialHealth);
             When.TriedToHeal(healer.Id, enemy.Id, heal: pointsToHeal);
             enemy.HealthCondition.CurrentHealth.Should().Be(initialHealth);
         }
